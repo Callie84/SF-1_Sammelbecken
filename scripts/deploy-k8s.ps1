a81f01411c5bb41e14bb6718d5e176bb92e8bc32
Callie84 <github-actions[bot]@users.noreply.github.com>
2025-10-31 14:55:35 +0100

    k8s: ingress + basis-secrets + deploy-script

---
+++ scripts/deploy-k8s.ps1
@@ -0,0 +1,8 @@
+﻿param(
+  [string]\ = "default"
+)
+
+kubectl apply -f k8s/secrets.yaml --namespace \
+kubectl apply -f k8s/price-service.yaml --namespace \
+kubectl apply -f k8s/backend.yaml --namespace \
+kubectl apply -f k8s/ingress-caddy.yaml --namespace \
